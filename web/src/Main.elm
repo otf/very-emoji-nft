35c49d88ab7ed6b08f520c7fc7d4f70b53025cc0
otf <otf@me.com>
2022-09-16 19:14:31 +0900

    Layout header and content in web.

--- web/src/Main.elm
+++ web/src/Main.elm
@@ -4,18 +4,36 @@ import Element exposing (..)
 import Element.Region as Region
 import Html exposing (Html)
 
+logo : Element msg
+logo =
+  el
+    [ width <| px 80
+    , height <| px 40
+    ] <|
+    el [ Region.heading 1 ] <|
+      text "Very Emoji"
+
+
+header : Element msg
+header =
+  row
+    [ width fill
+    , padding 20
+    , spacing 20
+    ]
+    [ logo
+    ]
+
+content : Element msg
+content =
+  column [ width fill ]
+    [
+    ]
+
 main : Html msg
 main =
-  layout [] <|
-    row 
-      [ height fill
-      , width fill ]
-      [ el 
-          [ centerX
-          , centerY
-          , Region.heading 1
-          ]
-          <|
-          text "Very Emoji" 
+  layout [ width fill, height fill] <|
+    column [ width fill ]
+      [ header
+      , content
       ]
-      
