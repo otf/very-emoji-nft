6deb7c52e7a1d5f4e520da3c7e0df895fd78428d
otf <otf@me.com>
2022-09-26 17:09:25 +0900

    Add polygons mumbai network to hardhat.

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,9 +1,19 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-abi-exporter"
+import * as dotenv from "dotenv"
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+
+  networks: {
+    mumbai: {
+      url: process.env.API_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
 };
 
 export default config;
