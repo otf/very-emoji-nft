8d057582897d7ac51d4716d9d8ec559a5776b5f1
otf <otf@me.com>
2022-09-26 16:40:08 +0900

    Remove shellHook from nix-shell. refs #2

--- shell.nix
+++ shell.nix
@@ -7,10 +7,4 @@ mkShellNoCC {
   nativeBuildInputs = [
     nodejs-18_x
   ];
-  shellHook = ''
-    npm install
-    npx playwright install
-    npx playwright install-deps
-    export PATH="$PWD/node_modules/.bin/:$PATH"
-  '';
 }
