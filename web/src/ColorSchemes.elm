b33520f668b6899d0aefd0eb8521d5c4121eafa9
otf <otf@me.com>
2022-10-03 19:59:10 +0900

    Outline a font for logo. refs #5

--- web/src/ColorSchemes.elm
+++ web/src/ColorSchemes.elm
@@ -8,8 +8,8 @@ backgroundColor =
     gradient
         { angle = 0
         , steps =
-            [ rgb255 199 201 248
-            , rgb255 230 234 255
+            [ rgb255 230 234 255
             , rgb255 255 186 219
+            , rgb255 199 201 248
             ]
         }
